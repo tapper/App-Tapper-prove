18e2f3cb6c2e80f2159d6f97b148b0e4d82e7647
Steffen Schwigon <ss5@renormalist.net>
2013-04-20 22:18:52 +0200

    An alternative 'prove' which reports results to Tapper

---
+++ lib/App/Tapper/prove.pm
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+package App::Tapper::prove;
+# ABSTRACT: Tapper - tprove - alternative 'prove' which reports results to Tapper
+
+=pod
+
+See L<tprove|bin/tprove>.
+
+=cut
+
+1;
